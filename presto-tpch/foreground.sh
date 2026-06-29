78eda5e8aafb642cd9e124f57b283e0ab938c97d
Saurabh Mahawar <saurabhmahawar111@gmail.com>
2026-06-29 14:02:01 +0530

    Update foreground.sh to clear terminal after environment setup

--- presto-tpch/foreground.sh
+++ presto-tpch/foreground.sh
@@ -2,4 +2,6 @@
 
 echo "⏳ Preparing your PrestoDB environment... (pulling Docker image)"
 while [ ! -f /tmp/presto-ready ]; do sleep 1; done
+clear
 echo "✅ Environment ready! Follow the steps to get started."
+
